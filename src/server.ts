828c10623911a5817cb7e9171191b072eb80bca8
marquesx19@gmail.com <marquesx19@gmail.com>
2024-04-02 06:15:19 -0300

    second-commit

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,42 @@
 import fastify from "fastify";
+import {z} from 'zod';
+import {PrismaClient} from '@prisma/client';
+
 
 const app = fastify();
 
-app.get('/', ()=>{
-    return 'Hello NLW Unite'
+const  prisma = new PrismaClient({
+    log:['query'],
 })
-app.get('/teste', ()=>{
-    return 'Hello teste'
+
+
+app.post('/events', async (request, reply)=>{
+    const createEventSchema = z.object({
+        title : z.string().min(4),
+        details: z.string().nullable(),
+        maximumAtendees: z.number().int().positive().nullable(),
+    })
+
+    const data = createEventSchema.parse(request.body)
+
+   const event =  await prisma.events.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAtendees,
+            slug: new Date().toString(),
+
+        },
+        
+    })
+
+    return{eventId: event.id}
+
 })
 
 
 app.listen({port: 3333}).then(()=>{
-    console.log('HTTPS sever runing')
+    console.log('HTTPS sever run ing')
 } )
 
 
